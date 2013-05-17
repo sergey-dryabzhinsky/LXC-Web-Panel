2e5e3c3c5ed52c1df0c623ea5e79429a7656c2cb
Sergey Dryabzhinsky <sergey@rusoft.ru>
2013-05-17 21:40:45 +0400

    Use default units from df, color label by percents

--- lwp/fs.py
+++ lwp/fs.py
@@ -31,6 +31,21 @@ def get_type(blkdev):
     return fsType
 
 
+def format_bytes_size(size_in_bytes):
+    unit = ''
+    for u in ('k', 'M', 'G', 'T'):
+        if size_in_bytes > 1024:
+            size_in_bytes /= 1024.0
+            unit = u
+        else:
+            break
+    if size_in_bytes < 10:
+        return "%.2f%s" % (size_in_bytes, unit,)
+    if size_in_bytes < 100:
+        return "%.1f%s" % (size_in_bytes, unit,)
+    return "%.0f%s" % (size_in_bytes, unit,)
+
+
 def get_usage_ext234(blkdev):
     '''
     Get filesystem usage in MB
@@ -54,10 +69,10 @@ def get_usage_ext234(blkdev):
             if line.startswith("Free blocks:"):
                 block_free = int(line.split(":")[1].strip())
 
-        result['total'] = block_count * block_size / 1024 / 1024
-        result['free'] = block_free * block_size / 1024 / 1024
-        result['used'] = result['total'] - result['free']
-        result['percent'] = "{}%".format(result['used'] * 100 / result['total'])
+        result['total'] = format_bytes_size(block_count * block_size)
+        result['free'] = format_bytes_size(block_free * block_size)
+        result['used'] = format_bytes_size((block_count - block_free) * block_size)
+        result['percent'] = "{}".format(int((block_count - block_free) * 100.0 / block_count))
     return result
 
 
@@ -66,7 +81,7 @@ def get_usage(blkdev):
         'total': 0,
         'used': 0,
         'free': 0,
-        'percent': '0%'
+        'percent': '0'
     }
     fsType = get_type(blkdev)
     if fsType in ("ext2", "ext3", "ext4"):
